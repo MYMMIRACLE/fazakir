007054969f84211951fcd1cd9e04db724bca5770
MUHAMMEDHAFEEZ <125500179+MUHAMMEDHAFEEZ@users.noreply.github.com>
2024-03-19 04:00:59 +0200

    Refactor MoshafScreen widget and implement pagination

--- fzaker/lib/view/screen/quran/surah/moshaf.dart
+++ fzaker/lib/view/screen/quran/surah/moshaf.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:arabic_numbers/arabic_numbers.dart';
 import 'package:fazakir/core/constant/color.dart';
 import 'package:fazakir/logic/quran_cubit/quran_cubit.dart';
@@ -9,20 +7,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MoshafScreen extends StatefulWidget {
-  const MoshafScreen({super.key});
+  const MoshafScreen({Key? key}) : super(key: key);
 
   @override
   State<MoshafScreen> createState() => _MoshafScreenState();
 }
 
 class _MoshafScreenState extends State<MoshafScreen> {
+  late ArabicNumbers arabicNumber;
+  late PageController _pageController;
+  final int linesPerPage = 15;
+
   @override
   void initState() {
     super.initState();
     BlocProvider.of<QuranCubit>(context).quranVerse();
+    arabicNumber = ArabicNumbers();
+    _pageController = PageController(initialPage: 0);
   }
-
-  ArabicNumbers arabicNumber = ArabicNumbers();
 
   @override
   Widget build(BuildContext context) {
@@ -31,25 +33,35 @@ class _MoshafScreenState extends State<MoshafScreen> {
         builder: (context, state) {
           if (state is QuranLoading) {
             return const Center(
-              child: BuidLoadingIndicator(),
-            );
+                // child: BuildLoadingIndicator(),
+                );
           }
           if (state is QuranSuccess) {
             return Container(
               color: MyColors.green.withOpacity(0.2),
               child: PageView.builder(
-                itemCount: 20,
-                itemBuilder: (context, index) {
+                controller: _pageController,
+                reverse: true, // Enable right-to-left scrolling
+                itemCount: ((state.quranDataList.length / linesPerPage)).ceil(),
+                itemBuilder: (context, pageIndex) {
+                  final startIndex = pageIndex * linesPerPage;
+                  final endIndex = startIndex + linesPerPage;
+                  final pageAyats = state.quranDataList.sublist(
+                    startIndex,
+                    endIndex.clamp(0, state.quranDataList.length),
+                  );
+
                   return ListView.builder(
-                      itemCount: state.quranDataList.length,
-                      itemBuilder: (context, index) {
-                        final moshaf = state.quranDataList[index];
-                        return CustomText(
-                          '${moshaf.ayat} (${arabicNumber.convert(moshaf.verseIDAr)})',
-                          textAlign: TextAlign.center,
-                          fontSize: 20,
-                        );
-                      });
+                    itemCount: pageAyats.length,
+                    itemBuilder: (context, index) {
+                      final moshaf = pageAyats[index];
+                      return CustomText(
+                        '${moshaf.ayat} (${arabicNumber.convert(moshaf.verseIDAr)})',
+                        textAlign: TextAlign.center,
+                        fontSize: 20,
+                      );
+                    },
+                  );
                 },
               ),
             );
